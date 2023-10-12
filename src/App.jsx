4a5f4c07d1e797f313573c8c2fd68f26edca6caf
katota1114 <katota1114@gmail.com>
2023-10-12 16:11:16 +0900

    station 6まで

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
 // DO NOT DELETE
 
+import { useState } from 'react'
 import './App.css'
 
 /**
  * @type {() => JSX.Element}
  */
 export const App = () => {
-  return (
-    <header>お試しのアプリ</header>
-  )
+  const [dogUrl, setDogUrl] = useState("https://images.dog.ceo/breeds/pug/n02110958_16337.jpg")
+
+  function handleClick() {
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then((response) => response.json())
+      .then((data) => setDogUrl(data.message));
+  }
+
+  return [
+    <header>お試しのアプリ</header>,
+    <p>ワンちゃんの画像で癒やされてね</p>,
+    <img className="dogImg" src={dogUrl} />,
+    <button onClick={handleClick}>更新</button>
+  ]
 }
