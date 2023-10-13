4dabbfd48d2c70aacac3b64909f6425d13dfb65b
katota1114 <katota1114@gmail.com>
2023-10-13 19:13:18 +0900

    Railway おわり！

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ export const App = () => {
 
   return [
     <Header/>,
-    <DogListContainer />,
-    <Description/>
+    <Description/>,
+    <DogListContainer />
   ]
 }
--- src/DogListContainer.jsx
+++ src/DogListContainer.jsx
@@ -1,23 +1,42 @@
 // @ts-check
 
-import { useEffect, useState } from "react"
-import BreedsSelect from "./BreedsSelect";
+import { useEffect, useState } from 'react'
+import BreedsSelect from './BreedsSelect'
+import DogImage from './DogImage'
 
 export const DogListContainer = () => {
   const [breeds, setBreeds] = useState({});
   const [selectedBreed, setSelectedBreed] = useState('');
+  const [selectedBreedDogs, setSelectedBreedDogs] = useState([]);
 
   function handleChange(event) {
-    setSelectedBreed(event.target.value);
+    setSelectedBreed(event.target.value)
+  }
+
+  const limit = 12
+
+  function handleFetchButton() {
+    fetch(
+      'https://dog.ceo/api/breed/' + selectedBreed + '/images/random/' + limit,
+    ).then(response => response.json())
+    .then(data => setSelectedBreedDogs(data.message))
   }
 
   useEffect(() => {
-    fetch("https://dog.ceo/api/breeds/list/all")
-      .then((response) => response.json())
-      .then((data) => setBreeds(data.message));
-  },[]);
+    fetch('https://dog.ceo/api/breeds/list/all')
+      .then(response => response.json())
+      .then(data => setBreeds(data.message))
+  }, [])
 
-  return <BreedsSelect breeds={breeds} selectedBreed={selectedBreed} handleChange={handleChange} />
+  return [
+    <BreedsSelect
+      breeds={breeds}
+      selectedBreed={selectedBreed}
+      handleChange={handleChange}
+    />,
+    <button onClick={handleFetchButton}>表示</button>,
+    selectedBreedDogs.map(imgUrl => <DogImage imageUrl={imgUrl}/>)
+  ]
 }
 
 export default DogListContainer
